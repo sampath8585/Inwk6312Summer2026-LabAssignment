c72e5b68eaede2cd2d542f8a7c827616c14a6fd8
sampath reddy <jv279482@dal.ca>
2026-05-13 16:55:38 -0300

    finished lab1

---
+++ templates-lab/src/task8.py
@@ -0,0 +1,26 @@
+# import Jinja2 library and PyYAML
+from jinja2 import Environment, FileSystemLoader
+import yaml
+
+# Declare template environment
+ENV = Environment(loader=FileSystemLoader('.'))
+def get_interface_speed(interface_name):
+    """ get_interface_speed returns the default Mbps value for a given
+    network interface by looking for certain keywords in the name
+    """
+    if 'gigabit' in interface_name.lower():
+        return 1000
+    if 'fast' in interface_name.lower():
+        return 100
+# Filters are added to the ENV object after declaration. Note that we're
+# actually passing in our "get_interface_speed" function and not running
+# it--the template engine will execute this function when we call
+# template.render()
+
+ENV.filters['get_interface_speed'] = get_interface_speed
+template = ENV.get_template("template-task8.j2")
+
+# We load our YAML file and pass it in to the template when rendering it.
+with open("data-task7.yml") as f:
+    interfaces = yaml.load(f, Loader=yaml.SafeLoader)
+    print(template.render(interface_list=interfaces))
